9f6a4a6c2c8a588553e2160e5cb70ae28a3e521c
Thomas Le <tle1321@gmail.com>
2022-07-12 15:53:32 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,91 @@
+// Assignment code here
+
+let passwordGenerator = ()=>{
+
+ // function to find the length of password and store it in a variable
+  let getLength = ()=>{
+    let selectedLength =  window.prompt("Please select a length you would like you password to be.(Must be 8-128 characters")
+    if( selectedLength >= 8 && selectedLength <= 128){
+      return selectedLength
+    } else {
+      window.alert("Please enter a valid number (8-128)")
+      return getLength()
+    }
+  }
+
+ //Calls the getLength function to find the wanted length of the function
+  let charCount = getLength();
+  console.log(charCount)
+
+  let promptQuestions = ()=>{
+    let lowerAns = window.confirm("Click OK to include the use of lowercase characters in your password. Click Cancel if you would not like to include lowercase characters.")
+    let upperAns = window.confirm("Click OK to include the use of uppercase characters in your password. Click Cancel if you would not like to include uppercase characters.")
+    let numericAns = window.confirm("Click OK to include the use of numeric characters in your password. Click Cancel if you would not like to include numeric characters.")
+    let specialAns = window.confirm("Click OK to include the use of special characters in your password. Click Cancel if you would not like to include special characters.")
+
+ //store answers selected in object
+    const ansObject = {
+      answer:[
+        lowerAns,
+        upperAns,
+        numericAns,
+        specialAns
+      ]
+    }
+
+    if(!lowerAns && !upperAns && !numericAns && !specialAns){
+      window.alert("Atleast one character type must be chosen. Please try again.")
+      return promptQuestions()
+    } else{
+      return ansObject
+    }
+  }
+  // Calls the promtQuestions function and stores it in an variable object
+  let ans = promptQuestions();
+  console.log(ans)
+
+  //Create string of potential characters
+  let lowerCharacters = 'abcdefghijklmnopqrstuvwxyz'
+  let upperCharacters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+  let numericCharacters = '1234567890'
+  let specialCharacters = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~"
+  let potentialCharacters = ''
+  let finalPassword = ''
+
+  if(ans.answer[0] === true){
+    potentialCharacters += lowerCharacters
+  }
+  if(ans.answer[1] === true){
+    potentialCharacters += upperCharacters
+  }
+  if(ans.answer[2] === true){
+    potentialCharacters += numericCharacters
+  }
+  if(ans.answer[3] === true){
+    potentialCharacters += specialCharacters
+  }
+
+  console.log(potentialCharacters)
+
+//Loops through potential characters and picks random character to add to final password.
+  for(let i = 0; i < charCount; i++){
+    finalPassword += potentialCharacters.charAt(Math.floor(Math.random() * potentialCharacters.length))
+    console.log(finalPassword)
+  }
+  return finalPassword
+}
+
+
+// Get references to the #generate element
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = passwordGenerator();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
